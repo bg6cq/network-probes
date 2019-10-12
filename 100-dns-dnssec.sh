aa6f85890dabcec179a3229cdc29a32bf10ac156
Zhang Huanjie <james@ustc.edu.cn>
2019-10-12 19:55:41 +0800

    using delv to do dnssec check

--- 100-dns-dnssec.sh
+++ 100-dns-dnssec.sh
@@ -15,11 +15,8 @@ cnt=1
 while [ $cnt -le 3 ]; do
 	let cnt++
 	echo -n check dns sec: $1 >> $out_file
-	dig +dnssec +multi $1 > /tmp/tmp.$$
-#	cat /tmp/tmp.$pid
-	grep NOERROR /tmp/tmp.$$ > /dev/null && grep flags /tmp/tmp.$$ | grep ad > /dev/null && grep RRSIG /tmp/tmp.$$ > /dev/null && echo ", dnssec ok" >> $out_file && rm -f /tmp/tmp.$$ && exit 0
+	delv $1 2>/dev/null | grep -v "negative response" | grep "fully validated" > /dev/null && echo ", dnssec ok" >> $out_file && exit 0
 	echo " "no dnssec >> $out_file
-	rm -f /tmp/tmp.$$
 done
 
 exit 1

a99bfce682c09e3ce8f7cf7821c33b755f6fc3c7
Zhang Huanjie <james@ustc.edu.cn>
2020-12-30 08:06:53 +0800

    useragent

--- 200-http-ipv6.sh
+++ 200-http-ipv6.sh
@@ -21,7 +21,7 @@ cnt=1
 while [ $cnt -le 3 ]; do
 	let cnt++
 	echo -n check http v6: $1 >> $out_file
-	x=$(curl -m $TIMEOUT -6 -i $1 2>/dev/null | awk '/^HTTP/ {print $1}')
+	x=$(curl -m $TIMEOUT -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36" -6 -i $1 2>/dev/null | awk '/^HTTP/ {print $1}')
 	if [ ! -z "$x" ]; then
 		echo ", get http response header" $x >> $out_file
 		exit 0
